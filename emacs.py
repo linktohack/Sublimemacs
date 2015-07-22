fae7efa2cac20ace2561e4e5f145b67e87d02575
Quang-Linh LE <linktohack@gmail.com>
2015-07-22 15:49:37 +0200

    Move _exec and _flaten out of Emacs class

--- emacs.py
+++ emacs.py
@@ -1,15 +1,17 @@
 import sublime, sublime_plugin
 from Emacs.libemacs import Emacs
 
+emacs = Emacs()
+
 class EmacsKillDaemonCommand(sublime_plugin.WindowCommand):
     def run(self):
-        Emacs().kill_daemon()
+        emacs.kill_daemon()
 
 class EmacsOpenCurrentFileCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         file_name = self.view.file_name()
         (row,col) = self.view.rowcol(self.view.sel()[0].begin())
-        Emacs().open_in_emacs(file_name, row+1, col+1)
+        emacs.open_file(file_name, row+1, col+1)
 
 class EmacsCallInteractivelyCommand(sublime_plugin.TextCommand):
     def run(self, edit, command):
@@ -22,7 +24,7 @@ class EmacsCallInteractivelyCommand(sublime_plugin.TextCommand):
         buffer_string = view.substr(buffer_region)
 
         interactive_command = "(call-interactively '%s)" % command
-        new_buffer_string = Emacs().eval_in_buffer_string(buffer_string,
-                                interactive_command, beg, end, file_ext=file_ext)
+        new_buffer_string = emacs.eval_in_buffer_string(buffer_string,
+                                                          interactive_command, beg, end, file_ext=file_ext)
 
         view.replace(edit, buffer_region, new_buffer_string)
--- libemacs.py
+++ libemacs.py
@@ -1,4 +1,4 @@
-import subprocess
+import os, subprocess
 import tempfile
 
 # Daemon Emacs working as a service
@@ -14,55 +14,56 @@ INIT_FILE = '~/Downloads/sublime.el'
 # version.
 ALTERNATE_EDITOR = ['open', '-a', '/opt/homebrew-cask/Caskroom/emacs-mac/emacs-24.5-z-mac-5.8/Emacs.app', '--args']
 
+def _flatten(*cmd):
+    flat_cmd = []
+    for c in cmd:
+        if not isinstance(c, str):
+            flat_cmd.extend(_flatten(*c))
+        else:
+            flat_cmd.append(c)
+    return flat_cmd
+
+def _exec(*cmd, wait=True):
+    print('cmd', cmd)
+    flat_cmd = _flatten(*cmd)
+    print('flat_cmd', flat_cmd)
+    pipe = subprocess.Popen(flat_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if wait:
+        stdout, stderr = pipe.communicate()
+        return pipe.returncode, stdout.decode('utf-8'), stderr.decode('utf-8')
+    else:
+        return None, '', ''
+
 class Emacs:
-    def init(self, client=EMACSCLIENT, emacs=EMACS, param=EMACS_PARAM,
+    def __init__(self, client=EMACSCLIENT, emacs=EMACS, param=EMACS_PARAM,
+             alternate_editor=ALTERNATE_EDITOR,
              socket=SOCKET, init_file=INIT_FILE):
         self.client = client
         self.emacs = emacs
         self.param = param
+        self.alternate_editor = alternate_editor
         self.socket = socket
         self.init_file = init_file
-        
-    def flatten_cmd(self, *cmd):
-        flat_cmd = []
-        for c in cmd:
-            if not isinstance(c, str):
-                flat_cmd.extend(self.flatten_cmd(*c))
-            else:
-                flat_cmd.append(c)
-        return flat_cmd
-
-    def subprocess_exit_code_and_output(self, *cmd, wait=True):
-        flat_cmd = self.flatten_cmd(*cmd)
-        pipe = subprocess.Popen(flat_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if wait:
-            stdout, stderr = pipe.communicate()
-            return pipe.returncode, stdout.decode('utf-8'), stderr.decode('utf-8')
-        else:
-            return None, '', ''
-
-    def emacsclient(self, *args, wait=True):
+    
+    def _maybe_start_emacs(self):
         # TODO: Expensive version
         #       Using socket directly should be much faster, no process, no command line...
-        check_cmd = [EMACSCLIENT, '-s', SOCKET, '-e', 'nil']
-        exit_code, _, _ = self.subprocess_exit_code_and_output(check_cmd)
+        check_cmd = [self.client, '-s', self.socket, '-e', 'nil']
+        exit_code, _, _ = _exec(check_cmd)
         if exit_code != 0:
-            self.subprocess_exit_code_and_output(EMACS, '-q', '-l', INIT_FILE, '--daemon')
-            if not os.path.exists(SOCKET):
+            _exec(self.emacs, '-q', '-l', self.init_file, '--daemon')
+            if not os.path.exists(self.socket):
                 raise Exception('Failed to start Emacs Daemon')
-        open_cmd = [EMACSCLIENT, '-s', SOCKET]
-        open_cmd.extend(list(args))
-        return self.subprocess_exit_code_and_output(open_cmd, wait)
 
-    def kill_daemon(self):
-        src = """(kill-emacs)"""
-        cmd = ['-e', src]
-        exit_code, _, _ = self.emacsclient(cmd)
+    def eval(self, *args, wait=True):
+        self._maybe_start_emacs()
+        open_cmd = [self.client, '-s', self.socket]
+        open_cmd.extend(list(args))
+        return _exec(open_cmd, wait)
 
     def eval_in_file(self, file_name, commands, mark=0, point=0):
         if point == mark: mark_active = 'nil'
         else: mark_active = 't'
-
         src = """
             (progn
               (find-file "%s")
@@ -75,7 +76,7 @@ class Emacs:
               (list (mark) (point) mark-active))
             """ % (file_name, mark, point, mark_active, commands)
         cmd = ['-e', src]
-        exit_code, _, _ = self.emacsclient(cmd)
+        exit_code, _, _ = self.eval(cmd)
         return exit_code
 
     def eval_in_buffer_string(self, buffer_string, commands, mark=0, point=0, file_ext='.txt'):
@@ -88,16 +89,19 @@ class Emacs:
             return f.read().decode('utf-8')
         # TODO: Remove temporary file
 
-    def open_in_emacs(self, file_name, row, col):
-        check_cmd = [EMACSCLIENT, '-e', '"t"']
-        exit_code, _, _ = self.subprocess_exit_code_and_output(check_cmd)
+    def open_file(self, file_name, row, col):
+        check_cmd = [self.client, '-e', 'nil']
+        exit_code, _, _ = _exec(check_cmd)
         if exit_code == 0:
-            open_cmd = [EMACSCLIENT, '-n', '+%d:%d'%(row, col), file_name]
+            open_cmd = [self.client, '-n', '+%d:%d'%(row, col), file_name]
         else:
-            if ALTERNATE_EDITOR:
-                open_cmd = [ALTERNATE_EDITOR, EMACS_PARAM, '+%d:%d'%(row, col), file_name]
+            if self.alternate_editor:
+                open_cmd = [self.alternate_editor, self.param, '+%d:%d'%(row, col), file_name]
             else:
-                open_cmd = [EMACS, EMACS_PARAM, '+%d:%d'%(row, col), file_name]
+                open_cmd = [self.emacs, self.param, '+%d:%d'%(row, col), file_name]
+        _exec(open_cmd, wait=False)
 
-        subprocess.Popen(self.flatten_cmd(open_cmd))
-
+    def kill(self):
+        src = """(kill-emacs)"""
+        cmd = ['-e', src]
+        exit_code, _, _ = self.eval(cmd)
